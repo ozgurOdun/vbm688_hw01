b8a48de568bbaf8ee228b1a4ec65f298afd57ee3
odinc <odinc@Ozgurs-MacBook-Pro.local>
2019-03-27 16:13:37 +0300

    manhattan fixed

--- search/search.go
+++ search/search.go
@@ -1,5 +1,10 @@
 package search
 
+import (
+	_ "fmt"
+	"math"
+)
+
 type State struct {
 	Board     [][]int
 	EmptyTile int
@@ -26,24 +31,17 @@ const SizeY = 3
 func ManhattanDistance(state [][]int, goal [][]int) int {
 	var dx, dy int
 	var sum int
-
-	for y0 := 1; y0 < 3; y0++ {
-		for x0 := 1; x0 < 3; x0++ {
-
-			for y1 := 1; y1 < 3; y1++ {
-				for x1 := 1; x1 < 3; x1++ {
-					if state[y0][x0] == goal[y1][x1] {
-						if x0-x1 < 0 {
-							dx = x1 - x0
-						} else {
-							dx = x0 - x1
-						}
-						if y0-y1 < 0 {
-							dy = y1 - y0
-						} else {
-							dy = y0 - y1
-						}
+	sum = 0
+	for y0 := 0; y0 < 3; y0++ {
+		for x0 := 0; x0 < 3; x0++ {
+			for y1 := 0; y1 < 3; y1++ {
+				for x1 := 0; x1 < 3; x1++ {
+					if state[y0][x0] == goal[y1][x1] && state[y0][x0] != 0 {
+						dx = int(math.Abs(float64(x0 - x1)))
+						dy = int(math.Abs(float64(y0 - y1)))
 						sum = sum + dx + dy
+						//fmt.Printf("x0: %d, y0:%d, sta: %d,,y0:%d,x0:%d,goa:%d\n", x0, y0, state[y0][x0], x1, y1, goal[y1][x1])
+						//fmt.Printf("dx:%d,dy:%d,sum:%d\n", dx, dy, sum)
 					}
 				}
 			}
