7a511b9c5f6cc3b1696f2738d53cdaf68a2cc62c
odinc <odinc@Ozgurs-MacBook-Pro.local>
2019-03-30 03:35:50 +0300

    getting ready for program args

--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 	"github.com/ozgurOdun/vbm688_hw01/astar"
 	"github.com/ozgurOdun/vbm688_hw01/search"
 	"github.com/ozgurOdun/vbm688_hw01/utils"
+	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Usage: hw01 bfs or hw01 astar!")
+		return
+	}
+	if strings.Compare(args[1], "bfs") == 0 {
+		fmt.Println("bfs")
+	} else if strings.Compare(args[1], "bfs") == 0 {
+		fmt.Println("astar")
+	} else {
+		fmt.Println("Usage: hw01 bfs or hw01 astar!")
+		return
+	}
 	fmt.Println("Starting...", time.Now())
 	goal := make([][]int, 9)
 	goal = utils.FillGoal()
